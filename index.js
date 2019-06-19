ce42df2e9a15573f0f2c44b0b9bb2e1765e6699e
dlim87 <dlim87@gmail.com>
2019-06-19 15:10:54 -0700

    fully reverted index.js to a working version

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
+// 'use strict';
+
+// const line = require('@line/bot-sdk');
+// const express = require('express');
+// import parseText from './parseText.js'
+// // create LINE SDK config from env variables
+// const config = {
+//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+//   channelSecret: process.env.CHANNEL_SECRET,
+// };
+
+// // create LINE SDK client
+// const client = new line.Client(config);
+
+// // create Express app
+// // about Express itself: https://expressjs.com/
+// const app = express();
+
+// // register a webhook handler with middleware
+// // about the middleware, please refer to doc
+// app.post('/callback', line.middleware(config), (req, res) => {
+//   Promise
+//     .all(req.body.events.map(handleEvent))
+//     .then((result) => res.json(result))
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).end();
+//     });
+// });
+
+// // event handler
+// function handleEvent(event) {
+//   console.log(event)
+//   if (event.type !== 'message' || event.message.type !== 'text') {
+//     // ignore non-text-message event
+//     return Promise.resolve(null);
+//   }
+//   const incomingMessage = parseText(event.message.text)
+//   let returnmessage
+  
+//   // if(incomingMessage === false) returnmessage = "invalid request"
+//   // else if (typeof incomingMessage==="string") returnmessage= incomingMessage
+//   // else returnmessage =`${incomingMessage['type']}ing ${incomingMessage['bracket']}`
+
+//   returnmessage = event.message.text.includes("test")?event.message.text:"sorry, I don't understand"
+//   console.log("====== sending response ======")
+//   console.log(returnmessage)
+
+//   const response = { type: 'text', text: returnmessage };
+//   // use reply API
+//   return client.replyMessage(event.replyToken, response);
+// }
+
+// // listen on port
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`listening on ${port}`);
+// });
+
+    
 'use strict';
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-import parseText from './parseText.js'
+
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -35,20 +95,12 @@ function handleEvent(event) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-  const incomingMessage = parseText(event.message.text)
-  let returnmessage
-  
-  // if(incomingMessage === false) returnmessage = "invalid request"
-  // else if (typeof incomingMessage==="string") returnmessage= incomingMessage
-  // else returnmessage =`${incomingMessage['type']}ing ${incomingMessage['bracket']}`
+  const returnmessage = event.message.text.includes("test")?event.message.text:"sorry, I don't understand"
+  // create a echoing text message
+  const echo = { type: 'text', text: returnmessage };
 
-  returnmessage = event.message.text.includes("test")?event.message.text:"sorry, I don't understand"
-  console.log("====== sending response ======")
-  console.log(returnmessage)
-
-  const response = { type: 'text', text: returnmessage };
   // use reply API
-  return client.replyMessage(event.replyToken, response);
+  return client.replyMessage(event.replyToken, echo);
 }
 
 // listen on port

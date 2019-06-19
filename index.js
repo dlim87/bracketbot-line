77540fdf7d8ac8140fa3c620d7cd3d1cb39d7ba8
dlim87 <dlim87@gmail.com>
2019-06-19 15:25:28 -0700

    trying with importing w/o ./

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-// import parseText from './parseText.js'
+import parseText from 'parseText.js'
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -36,28 +36,6 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
   let returnmessage
-  const validBrackets = ['1.9', '2.9', '3.9', '4.9', '5.9',
-  '1.8', '2.8', '3.8', '4.8', '5.8',
-  '1.7', '2.7', '3.7', '4.7', '5.7',
-  '1.6', '2.6', '3.6', '4.6', '5.6',
-  'SCL9','SCL8','SCL7','SCL6']
-  
-  function parseText(text) {
-    const trimmedText = text.trim().split(' ')
-    const textCommand = trimmedText[0].toLowerCase()
-    if (textCommand==="!check"||textCommand==="!update"){
-      const textBracket = trimmedText[1].toUpperCase()
-      let textObject = {}
-      textObject['type']=textCommand.slice(1)
-      
-      if(validBrackets.includes(textBracket)) {
-        textObject['bracket'] = textBracket
-      }
-      else return "please choose a valid bracket"
-      return textObject
-    }
-    else return false
-  }
   const incomingMessage = parseText(event.message.text)
   if(incomingMessage === false) returnmessage = "invalid request"
   else if (typeof incomingMessage==="string") returnmessage= incomingMessage
